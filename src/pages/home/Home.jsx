02628919fa05d347adadbe534613f0e461410c77
galihivan <137308573+galihivan@users.noreply.github.com>
2025-02-19 15:43:04 +0700

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import HeroSection from "@/components/herosection/HeroSection";
 import TrustedBy from "@/components/TrustedBy";
-import Footer from "@/components/footer";
+import Footer from "@/components/footer/footer";
 import Testimoni from "@/components/testimoni/Testimoni";
 import StepsSection from "@/components/stepsection/StepSection";
 
@@ -15,7 +15,7 @@ function Home() {
       <StepsSection />
       <Testimoni />
     </main>
-      <Footer />
+    <Footer />
  
     
     </>
