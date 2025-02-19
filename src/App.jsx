f45e99885ef9b8b7ec537d2be8bc3af11ecfadf8
galihivan <galihivan815@gmail.com>
2025-02-19 14:22:51 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import UserProfile from "./pages/userProfile/UserProfile";
 
-// Komponen utama App dengan pengecekan lokasi
+
 function AppContent() {
-  const location = useLocation();
-
-  // Cek apakah sedang di halaman login atau register
-  const hideNavbar = location.pathname === "/login" || location.pathname === "/register";
 
   return (
     <>
-      {!hideNavbar && <Navbar />} {/* Navbar hanya muncul jika tidak di halaman login/register */}
+    
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/user-profile" element={<UserProfile />} />
+      
       </Routes>
     </>
   );
