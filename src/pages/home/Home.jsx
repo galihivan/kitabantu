e978dfc15a8c49849d0121e4971c229758579021
galihivan <galihivan815@gmail.com>
2025-02-19 14:41:19 +0700

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,9 @@
-import Navbar from "../../components/navbar/Navbar";
-import HeroSection from "../../components/herosection/HeroSection";
-import TrustedBy from "../../components/TrustedBy";
-import Footer from "../../components/footer/Footer";
-import Testimoni from "../../components/testimoni/Testimoni";
-import StepsSection from "../../components/stepsection/StepSection";
-
-
+import Navbar from "@/components/navbar/Navbar";
+import HeroSection from "@/components/herosection/HeroSection";
+import TrustedBy from "@/components/TrustedBy";
+import Footer from "@/components/footer/Footer";
+import Testimoni from "@/components/testimoni/Testimoni";
+import StepsSection from "@/components/stepsection/StepSection";
 
 function Home() {
   return (
